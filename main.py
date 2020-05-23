806eb0a9c6b6458d18cdf062bd26531a37755f4f
Rob Wells <rob@robjwells.com>
2020-05-23 10:12:53 +0100

    Wrap in main function

--- main.py
+++ main.py
@@ -22,35 +22,43 @@ Or in short:
 """
 from datetime import datetime
 
-current_year = datetime.now().year
-al_year_start = datetime(current_year, month=1, day=1)
-al_year_end = datetime(current_year, month=12, day=31)
-al_year_length = (al_year_end - al_year_start).days + 1
+def main():
+    current_year = datetime.now().year
+    al_year_start = datetime(current_year, month=1, day=1)
+    al_year_end = datetime(current_year, month=12, day=31)
+    al_year_length = (al_year_end - al_year_start).days + 1
 
-al_for_full_year = float(input("How many days annual leave for the full year? "))
-try:
-    start_date = datetime.strptime(
-        input(
-            f"Employee start date in YYYY-MM-DD format [{al_year_start:%Y-%m-%d}]: "
-        ),
-        "%Y-%m-%d",
-    )
-except ValueError:
-    start_date = al_year_start
-try:
-    end_date = datetime.strptime(
-        input(
-            f"Employee finish date in YYYY-MM-DD format [{al_year_end:%Y-%m-%d}): "
-        ),
-        "%Y-%m-%d",
-    )
-except ValueError:
-    end_date = al_year_end
+    al_for_full_year = float(input(f"How many days annual leave for the full year? [{statutory_al}] "))
+    try:
+        start_date = datetime.strptime(
+            input(
+                f"Employee start date in YYYY-MM-DD format [{al_year_start:%Y-%m-%d}]: "
+            ),
+            "%Y-%m-%d",
+        )
+    except ValueError:
+        start_date = al_year_start
+    try:
+        end_date = datetime.strptime(
+            input(
+                f"Employee finish date in YYYY-MM-DD format [{al_year_end:%Y-%m-%d}): "
+            ),
+            "%Y-%m-%d",
+        )
+    except ValueError:
+        end_date = al_year_end
 
-al_period_length = (end_date - start_date).days + 1
-# +1 as we assume, eg, starting and leaving on Jan 1 accrues
-# 1 day's worth of leave, not zero
+    al_period_length = (end_date - start_date).days + 1
+    # +1 as we assume, eg, starting and leaving on Jan 1 accrues
+    # 1 day's worth of leave, not zero
 
-proportion_of_al_year_worked = al_period_length / al_year_length
-al_days_available = al_for_full_year * proportion_of_al_year_worked
-print(f"{round(al_days_available, 2)} days annual leave")
+    proportion_of_al_year_worked = al_period_length / al_year_length
+    al_days_available = al_for_full_year * proportion_of_al_year_worked
+    print(f"{round(al_days_available, 2)} days annual leave")
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except (KeyboardInterrupt, EOFError):
+        print()  # Prevent messing up some shells
